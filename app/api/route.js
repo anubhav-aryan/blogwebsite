116b893a3bf58fc85966b5e3b94ef33c5689d167
anubhav-aryan <aryananubhav08@gmail.com>
2023-09-28 02:20:10 +0530

    fix: pages

--- app/api/route.js
+++ app/api/route.js
@@ -1,7 +1,21 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
-import Post from "@models/post"
+import Post from "@/models/post"
+import { connectToDB } from "@utils/mongodb";
 
 export async function GET(req) {  
-    
+    const dbConnection = await connectToDB();
+    try {
+        const PostModel = await Post.find();
+        if (PostModel) {
+            return NextResponse.error(
+                new Error("No posts found"),
+            );
+        }
+        return NextResponse.json(PostModel);
+    } catch (error) {
+        return NextResponse.error(
+            new Error("Error Fetching Posts"),
+        );
+    }
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 "use client";
-// import BlogCard from "../components/BlogCard";
 import { useState, useEffect, useContext } from "react";
 // import PostContext from "../context/PostContext";
 import axios from "axios";
@@ -12,36 +11,39 @@ export default function Home() {
     {
       title: "How to use React",
       subheading: "React is a great framework",
-      image:
-        "https://dummyimage.com/640x360/fff/aaa",
+      image: "https://dummyimage.com/640x360/fff/aaa",
       tag: "React",
     },
   ]);
   const [selectedTags, setSelectedTags] = useState([]);
   const [error, setError] = useState(null);
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-  //       const data = await axios.get("/api/posts");
-  //       setPosts(data.data);
-  //     } catch (error) {
-  //       setError("Error Fetching Posts");
-  //     }
-  //   }
-  // }, []);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await axios.get("/api/posts");
+        setPosts(data.data);
+      } catch (error) {
+        setError("Error Fetching Posts");
+      }
+    }
+  }, []);
 
   if (posts?.length === 0) {
     return <div className="text-center">No posts found...</div>;
   }
 
-  // if (error) {
-  //   return (
-  //     <div className="text-center">
-  //       <img src="#" alt="Error" className="w-96" />
-  //     </div>
-  //   );
-  // }
+  if (error) {
+    return (
+      <div className="text-center">
+        <img
+          src="https://images.unsplash.com/photo-1682687980976-fec0915c6177?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+          alt="Error"
+          className="w-96"
+        />
+      </div>
+    );
+  }
 
   return (
     <>
@@ -76,21 +78,36 @@ export default function Home() {
             </button>
           )}
         </h2>
-        {selectedTags?.length === 0 &&
-          posts
-            ?.filter((post) => {
-              return selectedTags?.includes(post.tag);
-            })
-            .map((post) => {
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
+          {selectedTags?.length !== 0 &&
+            posts
+              ?.filter((post) => {
+                return selectedTags.includes(post.tag);
+              })
+              .map((post) => {
+                return (
+                  <BlogCard
+                    tag={post.tag}
+                    title={post.title}
+                    image={post.img}
+                    key={post.title}
+                    desc={post.subheading}
+                  />
+                );
+              })}
+          {selectedTags?.length === 0 &&
+            posts.map((post) => {
               return (
                 <BlogCard
                   tag={post.tag}
                   title={post.title}
-                  subheading={post.subheading}
-                  image={post.image}
+                  image={post.img}
+                  key={post.title}
+                  desc={post.subheading}
                 />
               );
             })}
+        </div>
       </div>
     </>
   );
